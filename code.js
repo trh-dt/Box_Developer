342a20d6b97d91a54a376e91c0bdbf3d63ddca48
trh-dt <terekh.d08@gmal.com>
2025-01-06 21:43:13 +0100

    edits

---
+++ code.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var ageCheck = prompt("Введите ваш возраст:");
+
+    if (ageCheck < 18) {
+        alert("Вам нет 18 лет, доступ запрещен!");
+    } else {
+        alert("Добро пожаловать!");
+    }
+});
